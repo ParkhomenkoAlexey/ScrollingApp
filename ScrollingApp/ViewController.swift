570b6877d7d0dae2796350e3d517529d943cba7d
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-09-30 02:01:01 +0300

    Initial Commit

---
+++ ScrollingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingApp
+//
+//  Created by Алексей Пархоменко on 30/09/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
