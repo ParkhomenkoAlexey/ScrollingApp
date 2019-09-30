393e0cbeec69d3213dc6682e1736bfdba9cf61b4
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-09-30 10:02:01 +0300

    Final project

--- ScrollingApp/ViewController.swift
+++ ScrollingApp/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageScrollView: ImageScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageScrollView = ImageScrollView(frame: view.bounds)
+        view.addSubview(imageScrollView)
+        setupImageScrollView()
+        
+        let imagePath = Bundle.main.path(forResource: "autumn", ofType: "jpg")!
+        let image = UIImage(contentsOfFile: imagePath)!
+        
+        self.imageScrollView.set(image: image)
+        
+    }
+    
+    func setupImageScrollView() {
+        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
+        imageScrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        imageScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
     }
 
 
